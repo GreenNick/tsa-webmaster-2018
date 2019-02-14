ca57a837a190142115f5014e6f9a0ac2385d8591
Nicholas Bowers <greennick7777@gmail.com>
2019-02-13 19:22:30 -0500

    adds cte landing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import TSA from './Components/TSA/TSA'
+import CTE from './Components/CTE/CTE'
 
 const App = () => (
   <BrowserRouter>
     <div>
       <Route path='/' component={TSA} exact />
+      <Route path='/CTE' component={CTE} exact />
     </div>
   </BrowserRouter>
 )
---
+++ src/Components/CTE/CTE.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Landing from '../Landing/Landing'
+
+const CTE = () => (
+  <Landing
+    title='Career and Technology/Engineering Program'
+    tagline='' />
+)
+
+export default CTE

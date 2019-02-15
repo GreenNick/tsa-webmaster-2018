f3adc93efc2cdfa8e237b16456582639e2ac9847
Nick Bowers <greennick7777@gmail.com>
2019-02-15 16:45:58 -0500

    adds hero image

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -4,7 +4,7 @@ import styles from './Landing.module.css'
 
 const Landing = ({ title, tagline }) => (
   <header className={styles.header}>
-    <div></div>
+    <div className={styles.hero}></div>
     <h1>{title}</h1>
     <p><em>{tagline}</em></p>
     <nav className={styles.nav}>

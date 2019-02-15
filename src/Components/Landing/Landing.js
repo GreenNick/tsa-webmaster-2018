c7358f26d9920dd40badb56e4692da4861d11f3e
Nick Bowers <greennick7777@gmail.com>
2019-02-15 16:34:28 -0500

    adds nav bar

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styles from './Landing.module.css'
 
 const Landing = ({ title, tagline }) => (
@@ -6,6 +7,13 @@ const Landing = ({ title, tagline }) => (
     <div></div>
     <h1>{title}</h1>
     <p><em>{tagline}</em></p>
+    <nav className={styles.nav}>
+      <ul>
+        <li><Link to='/'>TSA Chapter</Link></li>
+        <li><Link to='/CTE'>CTE Program</Link></li>
+        <li><Link to='/DesignBrief'>Design Brief</Link></li>
+      </ul>
+    </nav>
   </header>
 )
 

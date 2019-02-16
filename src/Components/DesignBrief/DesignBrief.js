2c34cd84a0e5e14e0fff0c18043f6b86b3e19ba2
1320D_Programmer <justinwadex@gmail.com>
2019-02-15 23:19:46 -0500

    biography

--- src/Components/DesignBrief/DesignBrief.js
+++ src/Components/DesignBrief/DesignBrief.js
@@ -13,7 +13,7 @@ const DesignBrief = () => (
       title='Biography'
       src='/Images/Jonah.jpg'
       alt=''>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et, quisquam sunt. Molestiae dolorum at autem veritatis illum tenetur quos, tempore atque quo possimus aliquid eaque! Eos dolor dolore dicta eveniet. In, accusantium quia corporis quod tenetur ducimus nam? Repellat vitae obcaecati numquam nihil voluptates modi repudiandae, eligendi facere fugit molestiae atque amet a ullam iste ratione in! Dolorem, facere dolor.</p>
+      <p>Jonah Fields began his musical path to greatness as a humble novice guitarist. While he played from time to time, he did not have the passion of a true musician. However, throughout his difficult high school years, he began to rely on music as a way to cope with calculus class. In time, this led him to grow far more skilled. Today, Jonah is a revolutionary metal guitarist who redefines music with each album. Hundreds of thousands of fans compete to attend his concerts, and his music is heavily analyzed by any who consider themselves well-versed in music. Jonah Fields is a modern prodigy, with all the class of Beethoven, the skill of Mozart, and the lyrical genius of Jake Paul.</p>
     </BrokenGrid>
     <Albums />
     <TourDates />

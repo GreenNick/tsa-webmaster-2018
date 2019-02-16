e2bf7e83c237a28ffff33f8c456a07abe0592493
1320D_Programmer <justinwadex@gmail.com>
2019-02-15 23:21:04 -0500

    tour prices

--- src/Components/TourDates/TourDates.js
+++ src/Components/TourDates/TourDates.js
@@ -8,32 +8,32 @@ const TourDates = () => (
       <div>
         <h3>Atlanta</h3>
         <p>March 1 2019<br />
-        Admission: $50</p>
+          Admission: $250</p>
       </div>
       <div>
         <h3>Athens</h3>
         <p>March 9 2019<br />
-        Admission: $40</p>
+          Admission: $200</p>
       </div>
       <div>
         <h3>Savannah</h3>
         <p>March 15 2019<br />
-        Admission: $50</p>
+          Admission: 230</p>
       </div>
       <div>
         <h3>Columbus</h3>
         <p>March 22 2019<br />
-        Admission: $35</p>
+          Admission: $170</p>
       </div>
       <div>
         <h3>Macon</h3>
         <p>April 5 2019<br />
-        Admission: $30</p>
+          Admission: $160</p>
       </div>
       <div>
         <h3>Augusta</h3>
         <p>April 13 2019<br />
-        Admission $30</p>
+          Admission $150</p>
       </div>
     </div>
   </section>
